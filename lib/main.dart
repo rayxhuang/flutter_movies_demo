76711d798311de5722bc9cd6ac26d03bfba8bcdf
rayhuang <rayxhuang2@gmail.com>
2021-04-20 17:04:15 +1200

    [Add]

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_demo/presentation/my_app.dart';
 
+const String gitToken = "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";
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MyAppWidget());
