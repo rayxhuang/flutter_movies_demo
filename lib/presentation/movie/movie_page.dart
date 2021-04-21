3d23f1409f788d236308129b4b4c8a4af5355bfa
rayhuang <rayxhuang2@gmail.com>
2021-04-21 15:59:09 +1200

    [Add] movie page animation

--- lib/presentation/movie/movie_page.dart
+++ lib/presentation/movie/movie_page.dart
@@ -5,7 +5,29 @@ import 'package:flutter_app_demo/domain/movie/movie.dart';
 import 'package:flutter_app_demo/infrastructure/movie/movie_repo.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class MoviePage extends StatelessWidget {
+class MoviePage extends StatefulWidget{
+  @override
+  _MoviePageState createState() => _MoviePageState();
+}
+
+class _MoviePageState extends State<MoviePage> with SingleTickerProviderStateMixin{
+  AnimationController _movieAnimationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _movieAnimationController = AnimationController(
+      duration: const Duration(milliseconds: 1500),
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _movieAnimationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
@@ -16,11 +38,21 @@ class MoviePage extends StatelessWidget {
       child: BlocBuilder<MovieBloc, MovieState>(
         builder: (BuildContext context, state) {
           if (state is MovieLoadedSuccessfulState) {
+            _movieAnimationController.forward();
             return Container(
-              child: GridView.count(
-                crossAxisCount: 2,
-                childAspectRatio: 0.618,
-                children: buildMovieGridViewChildren(state.movies),
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: MediaQuery.of(context).size.width / 2,
+                  childAspectRatio: 0.618,
+                ),
+                itemCount: state.movies.length,
+                itemBuilder: (context, index) {
+                  return MovieDisplayWidget(
+                    movie: state.movies[index],
+                    animationController: _movieAnimationController,
+                    offsetMultiplier: index ~/ 2,
+                  );
+                }
               ),
             );
           } else {
@@ -29,14 +61,6 @@ class MoviePage extends StatelessWidget {
         },
       )
     );
-  }
-
-  List<Widget> buildMovieGridViewChildren(List<MovieEntity> movies) {
-    List<Widget> widgetList = [];
-    for (MovieEntity movie in movies) {
-      widgetList.add(MovieDisplayWidget(movie: movie));
-    }
-    return widgetList;
   }
 }
 
@@ -60,16 +84,63 @@ class MovieLoadingWidget extends StatelessWidget {
 
 class MovieDisplayWidget extends StatelessWidget {
   final MovieEntity movie;
+  final AnimationController animationController;
+  final int offsetMultiplier;
   
-  MovieDisplayWidget({@required this.movie});
+  MovieDisplayWidget({@required this.movie, @required this.animationController, @required this.offsetMultiplier});
   
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: FittedBox(
-        fit: BoxFit.fill,
-        child: Image.network(this.movie.imageURL),
+    final Animation<double> _animationOpacity = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: animationController,
+        curve: Curves.easeInQuart,
       )
+    );
+
+    final Animation<Offset> _animationOffset = Tween<Offset>(begin: Offset(0, offsetMultiplier + 1.toDouble()), end: const Offset(0, 0)).animate(
+        CurvedAnimation(
+          parent: animationController,
+          curve: Curves.easeInOut,
+        )
+    );
+
+    return FadeTransition(
+      opacity: _animationOpacity,
+      child: SlideTransition(
+        position: _animationOffset,
+        child: Container(
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              Image.network(
+                this.movie.imageURL,
+                fit: BoxFit.fill,
+                errorBuilder: (context, dynamic, error) =>
+                  Image.asset(
+                    'assets/images/error-image.png',
+                    fit: BoxFit.fill,
+                  ),
+              ),
+              Positioned(
+                left: 12,
+                right: 12,
+                bottom: 20,
+                child: Text(
+                  '${this.movie.title}',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
